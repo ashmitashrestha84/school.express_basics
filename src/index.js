20376abac656fa165315fa0b7817917f66976f54
ashmitashrestha84 <ashmitashrestha247@gmail.com>
2026-06-29 18:52:24 +0545

    add:RoutingAPI of Student

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import http from "http";
 import express from "express";
+import studentRoutes from "./routes/student.routes.js";
 
 const app = express();
 
 app.use(express.json());
 
+
 const server = http.createServer(app);
 
-// Arrays
-const students = [];
 const marks = [];
 
 // Home Route
@@ -19,115 +19,7 @@ app.get("/", (req, res) => {
     });
 });
 
-
-// ===================== STUDENT ROUTES =====================
-
-// Get all students
-app.get("/students", (req, res) => {
-    res.status(200).json({
-        message: "Students fetched successfully",
-        success: true,
-        data: students,
-    });
-});
-
-// Get student by ID
-app.get("/students/:id", (req, res) => {
-    const { id } = req.params;
-
-    const student = students.find(
-        (student) => student.id === Number(id)
-    );
-
-    if (!student) {
-        return res.status(404).json({
-            message: "Student not found",
-            success: false,
-            data: null,
-        });
-    }
-
-    res.status(200).json({
-        message: "Student fetched successfully",
-        success: true,
-        data: student,
-    });
-});
-
-// Create student
-app.post("/students", (req, res) => {
-    const { name, faculty } = req.body;
-
-    students.push({
-        id: students.length + 1,
-        name,
-        faculty,
-        createdAt: Date.now(),
-    });
-
-    res.status(201).json({
-        message: "Student created successfully",
-        success: true,
-        data: students[students.length - 1],
-    });
-});
-
-// Update student
-app.put("/students/:id", (req, res) => {
-    const { id } = req.params;
-    const { name, faculty } = req.body;
-
-    const index = students.findIndex(
-        (student) => student.id === Number(id)
-    );
-
-    if (index === -1) {
-        return res.status(404).json({
-            message: "Student not found",
-            success: false,
-            data: null,
-        });
-    }
-
-    students[index] = {
-        ...students[index],
-        name,
-        faculty,
-    };
-
-    res.status(200).json({
-        message: "Student updated successfully",
-        success: true,
-        data: students[index],
-    });
-});
-
-// Delete student
-app.delete("/students/:id", (req, res) => {
-    const { id } = req.params;
-
-    const index = students.findIndex(
-        (student) => student.id === Number(id)
-    );
-
-    if (index === -1) {
-        return res.status(404).json({
-            message: "Student not found",
-            success: false,
-            data: null,
-        });
-    }
-
-    students.splice(index, 1);
-
-    res.status(200).json({
-        message: "Student deleted successfully",
-        success: true,
-        data: null,
-    });
-});
-
-
+app.use("/students", studentRoutes);
 // ===================== MARKS ROUTES =====================
 
 // Get all marks
---
+++ src/routes/student.routes.js
@@ -0,0 +1,95 @@
+import express from "express";
+const router=express.Router();
+const students = [];
+router.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Students fetched successfully",
+        success: true,
+        data: students,
+    });
+});
+
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+    const student = students.find(
+        student => student.id === Number(id)
+    );
+
+    if (!student) {
+        return res.status(404).json({
+            message: "Student not found",
+            success: false,
+            data: null,
+        });
+    }
+
+    res.status(200).json({
+        message: "Student fetched successfully",
+        success: true,
+        data: student,
+    });
+});
+
+router.post("/", (req, res) => {
+    const { name, faculty } = req.body;
+
+    students.push({
+        id: students.length + 1,
+        name,
+        faculty,
+        createdAt: Date.now(),
+    });
+
+
+    res.status(200).json({
+        message: "Student created successfully",
+        success: true,
+        data: students[students.length-1],
+    });
+});
+
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const { name, faculty } = req.body;
+    const index = students.findIndex(
+        student => student.id === Number(id)
+    );
+    if (index === -1) {
+        return res.status(404).json({
+            message: "Student not found",
+            success: false,
+            data: null,
+        });
+    }
+    students[index] = {
+        ...students[index],
+        name,
+        faculty,
+    };
+    res.status(200).json({
+        message: "Student updated successfully",
+        success: true,
+        data: students[index],
+    });
+});
+router.delete("/:id",(req,res)=>{
+       const {id}=req.params;
+       const index=students.findIndex((student)=> student.id=== Number(id));
+       if(index===-1){
+            res.status(404).json({
+            message:"user not found",
+            success:false,
+            data:null,
+        })
+        return;
+       }
+       students.splice(index,1);
+    res.status(200).json({
+        message: "Student deleted successfully",
+        success: true,
+        data: null,
+    });
+});
+
+export default router;
