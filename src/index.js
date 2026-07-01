f1946df1a5f4572c5a559245868d91b7cdbc0964
ashmitashrestha84 <ashmitashrestha247@gmail.com>
2026-07-01 14:15:58 +0545

    add:middleware in index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,24 @@ const app = express();
 app.use(express.json());
 
 const server = http.createServer(app);
-
+app.use((req,res,next)=>{
+    console.log("middleware1"),
+    req.user={
+        id:1,
+    }
+    next();
+})
+app.use((req,res,next)=>{
+    console.log("middleware2");
+    if(req.body){
+        next();
+    }else{
+           res.status(401).json({
+      message:"Unauthorized. Access denied",
+    })
+  }
+  next();
+});
 app.get("/", (req, res) => {
     res.status(200).json({
         message: "Server is up and running",
@@ -23,3 +40,13 @@ app.use("/subjects",subjectRoutes);
 server.listen(8080, "localhost", () => {
     console.log("Server running at http://localhost:8080");
 });
+
+
+app.use((err,req,res,next)=>{
+console.log(err);
+  res.status(err?.statusCode ?? 500).json({
+    message: err?.message ?? "something went wrong",
+    success:false,
+    data:null,
+  })
+})
